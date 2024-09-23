ec12c41819734d1fac296d7c53e72e0c13db7243
Alex Quennelle <bluephoenixcg@gmail.com>
2024-09-23 15:31:46 -0500

    Manually format

--- Assets/Scripts/Movers/PointAt.cs
+++ Assets/Scripts/Movers/PointAt.cs
@@ -5,7 +5,7 @@ public class PointAt : MonoBehaviour
 	//the target transform to be pointed at
 	public Transform target;
 	[
-		Range(0,180),
+		Range(0, 180),
 		Tooltip(
 				"The maximum number of degrees the object can rotate per update" + "\n" +
 				"The lower the number, the slower the object rotates" + "\n" +
@@ -21,7 +21,7 @@ public class PointAt : MonoBehaviour
 		Vector2 targetV = (target.position - transform.position).normalized;
 		//if the maxAngle variable is 0, then set the object's transform.up vector to be the target vector, and return
 		//this causes the object to instantly rotate to point at the target, and exits the function early
-		if(maxAngle==0)
+		if(maxAngle == 0)
 		{
 			transform.up = targetV;
 			return;
@@ -30,6 +30,6 @@ public class PointAt : MonoBehaviour
 		//this function does exactly what the name implies, and returns a vector rotated towards matching the target
 		//the returned vector will never be more than the max angle degrees away from the initial up transform
 		//by storing this new vector back into transform.up, the rotation is applied to the object
-		transform.up = Vector3.RotateTowards(transform.up,targetV,Mathf.Deg2Rad*maxAngle,0);
+		transform.up = Vector3.RotateTowards(transform.up, targetV, Mathf.Deg2Rad * maxAngle, 0);
 	}
 }
